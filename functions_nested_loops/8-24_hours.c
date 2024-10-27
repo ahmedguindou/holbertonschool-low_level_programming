bd9b5133de5344aa1f057b7aaa658f4bbe1dc4d5
ahmedguindou <guindouahmed@gmail.com>
2024-10-27 12:06:21 +0100

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day from 00:00 to 23:59
+ *
+ * This function prints the time in hours and minutes,
+ * in the format HH:MM, for every minute in a 24-hour period.
+ * It uses nested loops to iterate over hours and minutes,
+ * and prints each minute on a new line.
+ *
+ * Return: void (does not return a value)
+ */
+int jack_bauer(void)
+{
+int h;
+int m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
+_putchar(':');
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
+_putchar('\n');
+}
+}
+}
