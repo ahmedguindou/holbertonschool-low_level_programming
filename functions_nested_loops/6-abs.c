67c15c4a6a204d07435970a7b54eaeea2fe103a7
ahmedguindou <guindouahmed@gmail.com>
2024-10-27 11:15:21 +0100

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number whose sign will be printed
+ *
+ * Return: 1 if n is greater than zero,
+ *         0 if n is zero,
+ *        -1 if n is less than zero
+ */
+int _abs(int n)
+{
+if (n < 0){
+return -n;
+}
+return (n);
+}
