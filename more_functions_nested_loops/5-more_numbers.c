8155e7c9ac91191ef5104060cf9c6a525b187a98
ahmedguindou <guindouahmed@gmail.com>
2024-10-29 11:47:44 +0100

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+char count, num;
+for (count = 0 ; count <= 10 ; count++)
+{
+for (num = 0 ; num <= 14 ; num++)
+{
+if (num >= 10)
+{
+_putchar('1');
+}
+_putchar(num % 10 + '0');
+}
+_putchar('\n');
+}
+}
