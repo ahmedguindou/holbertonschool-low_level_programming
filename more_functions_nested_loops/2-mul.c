40480b9ea9fcf688e850ac22a01e19280df4996b
ahmedguindou <guindouahmed@gmail.com>
2024-10-29 10:24:55 +0100

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - Checks if a character is a digit (0 through 9)
+ * @c: The character to check
+ *
+ * Return: 1 if the character is a digit, 0 otherwise
+ */
+int mul(int a, int b)
+{
+
+return a*b;
+}
