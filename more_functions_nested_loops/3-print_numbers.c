a30eda7f90f38255bf79152af9892b27679cfb0b
ahmedguindou <guindouahmed@gmail.com>
2024-10-29 10:54:09 +0100

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first integer to be multiplied
+ * @b: The second integer to be multiplied
+ * Return: The result of the multiplication of a and b
+ */
+void print_numbers(void)
+{
+char num;
+for (num = '0' ; num <= '9' ; num++ )
+{
+_putchar(num);
+}
+_putchar('\n');
+}
