fcc9afd6ca942f40bc0ecebe80f2dabcbe1c9425
ahmedguindou <guindouahmed@gmail.com>
2024-10-29 10:39:40 +0100

    test

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,13 +3,9 @@
  * mul - Multiplies two integers
  * @a: The first integer to be multiplied
  * @b: The second integer to be multiplied
- * @c: The third integer is the result
- *
  * Return: The result of the multiplication of a and b
  */
 int mul(int a, int b)
 {
-int c ;
-c = a * b;
-return (c);
+return (a * b);
 }
