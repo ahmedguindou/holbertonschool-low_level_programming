b14af7f1c4984e61e5d5dfac61d411e26ea37ac7
ahmedguindou <guindouahmed@gmail.com>
2024-10-29 12:04:09 +0100

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14
+ * Description: This function prints the numbers from 0 to 14,
+ * ten times, followed by a new line. Each number is printed
+ * without leading spaces and is followed by a newline character
+ * after each complete set.
+ */
 void more_numbers(void)
 {
 char count, num;
