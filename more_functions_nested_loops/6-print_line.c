8d2ce2054649a77d20aa2c62304faa54225f6934
ahmedguindou <guindouahmed@gmail.com>
2024-10-30 09:54:40 +0100

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * more_numbers - Prints 10 times the numbers from 0 to 14
- * Description: This function prints the numbers from 0 to 14,
- * ten times, followed by a new line. Each number is printed
- * without leading spaces and is followed by a newline character
- * after each complete set.
+ * print_line - Draws a straight line in the terminal.
+ * @n: The number of times the character '_' should be printed.
+ *
+ * Description: If n is 0 or less, the function should only print a new line.
  */
 void print_line(int n)
 {
-    char i;
-    for(i = 0 ; i < n ; i++ ){
-        _putchar('_');
-    }
-    _putchar('\n');
+char i;
+for (i = 0 ; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
