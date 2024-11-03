7bc0150dacf2311781b91add74c4135c5e5e0aea
ahmedguindou <guindouahmed@gmail.com>
2024-11-03 22:34:12 +0100

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - check the code
+ * Description: compare two strings :)
+ * @s1 : pointer to string1
+ * @s2 : pointer to string2
+ * Return: int
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int res = 0;
+while (*s1 != '\0' && *s2 != '\0' && res == 0)
+{
+res = *s1 - *s2;
+s1++;
+s2++;
+}
+return (res);
+}
