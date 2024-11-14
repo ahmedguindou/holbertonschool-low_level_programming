c508dddb2c91696216a6001c4eed03754df3f812
ahmedguindou <guindouahmed@gmail.com>
2024-11-14 15:24:55 +0100

    malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,31 +1,32 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * str_concat - Concatenates two strings.
- * @s1: The first string.
- * @s2: The second string.
- *
- * Return: A pointer to the concatenated string, or NULL on failure.
+ * _strdup - duplicates a string.
+ * @str: the string to be duplicated.
+ * Return: pointer to the duplicated string.
+ * or if memory allocation fails.
  */
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-char *concatenated;
-int len1 = 0, len2 = 0, i, j;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[len1])
-len1++;
-while (s2[len2])
-len2++;
-concatenated = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-if (concatenated == NULL)
+char *duplicate;
+unsigned int i, length = 0;
+if (str == NULL)
+{
 return (NULL);
-for (i = 0; s1[i]; i++)
-concatenated[i] = s1[i];
-for (j = 0; s2[j]; j++)
-concatenated[i + j] = s2[j];
-concatenated[i + j] = '\0';
-return (concatenated);
+}
+while (str[length] != '\0')
+{
+length++;
+}
+duplicate = (char *)malloc((length + 1) * sizeof(char));
+if (duplicate == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < length; i++)
+{
+duplicate[i] = str[i];
+}
+duplicate[length] = '\0';
+return (duplicate);
 }
