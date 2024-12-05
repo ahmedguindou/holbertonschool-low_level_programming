53d6a04413b39cfc836e8e166152925685d7e4b5
ahmedguindou <guindouahmed@gmail.com>
2024-12-05 09:46:17 +0100

    Descriptive commit message

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,17 @@
  */
 int main(void)
 {
-int i, j;
-
-for (i = 0; i <= 8; i++)
+int n ,j;
+for (n = 0; n <= 9; ++n)
+for (j = i +i)
 {
-for (j = i + 1; j <= 9; j++)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (i != 8 || j != 9)
+putchar(n + '0');
+if (n < 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
-}
 putchar('\n');
-
 return (0);
 }
