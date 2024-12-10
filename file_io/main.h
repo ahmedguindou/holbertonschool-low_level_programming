bc892dfb7973e2ac10cb6197506c2bdb09b0feff
ahmedguindou <guindouahmed@gmail.com>
2024-12-10 09:50:39 +0100

    betty

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <stdio.h>
-#ifndef LISTS_H
-#define LISTS_H
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
